f7ae9f39ed53e443afd2784bc1e9881a21af8ae3
sskim <tjdtjq91@gmail.com>
2020-12-04 18:20:25 +0900

    Initial Commit

---
+++ src/main/java/chap02/Calculator.java
@@ -0,0 +1,11 @@
+package chap02;
+
+/**
+ * @author sskim
+ */
+public class Calculator {
+
+    public static int plus(int a1, int a2) {
+        return a1 + a2;
+    }
+}
---
+++ src/test/java/chap02/CalculatorTest.java
@@ -0,0 +1,18 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author sskim
+ */
+public class CalculatorTest {
+
+    @Test
+    void plus() {
+        int result = Calculator.plus(1, 2);
+        assertEquals(3, result);
+        assertEquals(5, Calculator.plus(4, 1));
+    }
+}
